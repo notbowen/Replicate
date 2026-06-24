4a9b73793fdb945ba228007a4b1815faa87331b2
Hu Bowen <contact@hubowen.dev>
2026-06-24 19:58:53 +0800

    Initial Commit

---
+++ Replicate/ReplicateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReplicateApp.swift
+//  Replicate
+//
+//  Created by Hu Bowen on 24/6/26.
+//
+
+import SwiftUI
+
+@main
+struct ReplicateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
